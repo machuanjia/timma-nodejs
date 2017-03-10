77f4cc09f870b9e69b48f2597639281842c499f6
machuanjia <machuanjia@worktile.com>
2017-03-10 10:19:07 +0800

    add blog

---
+++ myblog/middlewares/check.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yanshi0429 on 17/3/9.
+ */
+module.exports = {
+   checkLogin : function checkLogin(req,res,next) {
+       if(!req.session.user){
+           req.flash('error','未登录');
+           return res.redirect('/signin');
+       }
+       next();
+   },
+    checkNotLogin: function checkNotLogin(req,res,next){
+       if(req.session.user){
+           req.flash('error','已登录');
+           return res.redirect('back');
+       }
+       next();
+    }
+};
